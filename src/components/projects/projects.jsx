98b30301961f48268b6de4396eca9f07e0df6b84
erkanuz <erkan.nedim.999@gmail.com>
2023-03-17 19:14:43 +0200

    Portfolio

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,65 +1,60 @@
-import React from 'react'
+import React ,{ useState, useEffect} from 'react'
 import './projects.css'
+
+import { db } from '../../firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { query, orderBy } from "firebase/firestore"; 
 
 import {GoTools} from 'react-icons/go'
 import {FaLink} from 'react-icons/fa'
-import { useState } from 'react'
-import Menu from './Menu'
 
 import { motion } from 'framer-motion'
+import { card } from '../anima/animation'
 import { useScroll } from '../anima/useScroll'
-import { card, filter} from '../anima/animation'
 
 const Projects = () => {
-  const [element, controls] = useScroll();
-  const [a,b] = useState(Menu);
-  const e = (u) => {
-    const update = Menu.filter((c) => {
-      return c.category === u;
-    });
-    b(update);
-  }
-  
+    const [element, controls] = useScroll();
+    const [projects, setProjects] = useState([]);
+    const projCollectionRef = collection(db, "projects");
+
+    useEffect(() => {
+        const getProjects = async () => {
+            const q = query(projCollectionRef, orderBy("category", "desc"));
+            const data = await getDocs(q);
+            setProjects(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+        };
+        getProjects();
+        //eslint-disable-next-line
+    }, []); 
+
   return (
     <section id='projects' className='projects section'>
       <h2 className='proj_title'>Recent Works</h2>
       <h5 className='proj_subtitle'>My Projects <GoTools/></h5>
       <div className='container'>
-      <motion.div className="filter"
-      variants={filter}
-      animate={controls}
-      transition={{delay: .6, duration: .9, type: "tween"}}
-      >
-        <span className="item" onClick={() => b(Menu)}>Everything</span>
-        <span className="item" onClick={() => e("Web")}>Web</span>
-        <span className="item" onClick={() => e("Mobile")}>Mobile</span>
-        <span className="item" onClick={() => e("3D Objects")}>3D Objects</span>
-      </motion.div>
-
-      <motion.div className="pro_container grid" ref={element}
-      variants={card}
-      animate={controls}
-      transition={{delay: .6, duration: .9, type: "tween"}}
-      >
-        {a.map((x) => {
-          const{ id, image, title, description, category, link} = x;
+        <motion.div className="pro_container grid" ref={element}
+        variants={card}
+        animate={controls}
+        transition={{delay: .6, duration: .9, type: "tween"}}
+        >
+        {projects.map((x) => {
           return(
-            <div className="card" key={id}>
+            <div className="card" key={x.id}>
               <div className="thumbnail">
-                <img src={image} alt="" className="img_work" />
+                <img src={x.image} alt="" className="img_work" />
                 <div className="mask"></div>
               </div>
 
-              <span className="category">{category}</span>
-              <h3 className="pro_title">{title}</h3>
-              <h3 className="pro_description">{description}</h3>
-              <a href={link} className="pro_button">
+              <span className="category">{x.category}</span>
+              <h3 className="pro_title">{x.title}</h3>
+              <h3 className="pro_description">{x.description}</h3>
+              <a href={x.link} className="pro_button">
                 <i className="pro_button_link"><FaLink/></i>
               </a>
             </div>
           )
         })}
-      </motion.div>
+        </motion.div>
     </div>
   </section>
   )
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export default app;
